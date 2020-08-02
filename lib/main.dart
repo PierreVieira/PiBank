d51b771625b2bdfd130e21b76c90577ccdd9f452
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2020-08-02 14:22:27 -0300

    [+] Contact Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,21 @@
 import 'package:flutter/material.dart';
+import 'screens/dashboard.dart';
 
-void main() {
-  runApp(PiBank());
-}
+void main() => runApp(PiBank());
 
 class PiBank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Color(0xFF030660),
+          accentColor: Color(0xFF212485),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Color(0xFF030660),
+            textTheme: ButtonTextTheme.primary,
+          )),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('PiBank'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('images/pi_bank_logo.jpg'),
-            Container(
-              height: 120,
-              width: 100,
-              color: Colors.blue,
-              child: Column(
-                children: <Widget>[
-                  Icon(Icons.people),
-                  Text('Contacts')
-                ],
-              ),
-            )
-          ],
-        )
-      ),
+      home: Dashboard(),
     );
   }
 }
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:pibank/screens/contact_form.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Pierre',
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => ContactForm()));
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
